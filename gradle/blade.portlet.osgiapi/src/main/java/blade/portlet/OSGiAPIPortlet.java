bd934cf90eccdf67720d6907a4c318f8bf0df086
Gregory Amerson <gregory.amerson@liferay.com>
2015-02-18 04:18:44 +0800

    normalize packages

--- gradle/blade.portlet.osgiapi/src/main/java/blade/portlet/OSGiAPIPortlet.java
+++ gradle/blade.portlet.osgiapi/src/main/java/blade/portlet/OSGiAPIPortlet.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package blade.portlet.osgiapi;
+package blade.portlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
