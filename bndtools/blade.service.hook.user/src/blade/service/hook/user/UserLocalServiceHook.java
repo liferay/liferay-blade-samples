85a9abe9543947e48ce90c50701c45edc3fc7f0a
Gregory Amerson <gregory.amerson@liferay.com>
2015-04-21 12:06:01 +0800

    [bndtools] added blade.service.hook.user

---
+++ bndtools/blade.service.hook.user/src/blade/service/hook/user/UserLocalServiceHook.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package blade.service.hook.user;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceWrapper;
+import com.liferay.portal.service.UserLocalServiceWrapper;
+
+@Component(
+		service = ServiceWrapper.class
+)
+public class UserLocalServiceHook extends UserLocalServiceWrapper {
+
+	public UserLocalServiceHook() {
+		super(null);
+	}
+
+	@Override
+	public User getUser(long userId) throws PortalException {
+		System.out.println("Getting user by id " + userId);
+		return super.getUser(userId);
+	}
+
+	@Override
+	public int authenticateByEmailAddress(long companyId, String emailAddress,
+			String password, Map<String, String[]> headerMap,
+			Map<String, String[]> parameterMap, Map<String, Object> resultsMap)
+			throws PortalException {
+		System.out.println("Authenticating user by email address " + emailAddress);
+		return super.authenticateByEmailAddress(companyId, emailAddress, password,
+				headerMap, parameterMap, resultsMap);
+	}
+
+}
