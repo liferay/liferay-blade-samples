5a3be08ec4d22e67f42f2937fd325a97003fe090
Gregory Amerson <gregory.amerson@liferay.com>
2015-05-25 13:49:34 +0800

    [maven] initial service builder example

---
+++ maven/blade.servicebuilder/blade.servicebuilder.api/src/main/java/com/liferay/sampleservicebuilder/service/FooServiceWrapper.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sampleservicebuilder.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link FooService}.
+ *
+ * @author Brian Wing Shun Chan
+ * @see FooService
+ * @generated
+ */
+@ProviderType
+public class FooServiceWrapper implements FooService,
+	ServiceWrapper<FooService> {
+	public FooServiceWrapper(FooService fooService) {
+		_fooService = fooService;
+	}
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	@Override
+	public java.lang.String getBeanIdentifier() {
+		return _fooService.getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	@Override
+	public void setBeanIdentifier(java.lang.String beanIdentifier) {
+		_fooService.setBeanIdentifier(beanIdentifier);
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedService}
+	 */
+	@Deprecated
+	public FooService getWrappedFooService() {
+		return _fooService;
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #setWrappedService}
+	 */
+	@Deprecated
+	public void setWrappedFooService(FooService fooService) {
+		_fooService = fooService;
+	}
+
+	@Override
+	public FooService getWrappedService() {
+		return _fooService;
+	}
+
+	@Override
+	public void setWrappedService(FooService fooService) {
+		_fooService = fooService;
+	}
+
+	private FooService _fooService;
+}
