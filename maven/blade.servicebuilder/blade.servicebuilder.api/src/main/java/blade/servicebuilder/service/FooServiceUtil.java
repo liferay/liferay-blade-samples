87aa9d3344604b1ce50b4e180ab570dfa52a0f76
Andy Wu <andy.wu@liferay.com>
2015-05-28 17:36:02 +0800

    [maven]serviceBuilderTest not success

---
+++ maven/blade.servicebuilder/blade.servicebuilder.api/src/main/java/blade/servicebuilder/service/FooServiceUtil.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package blade.servicebuilder.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+
+/**
+ * Provides the remote service utility for Foo. This utility wraps
+ * {@link blade.servicebuilder.service.impl.FooServiceImpl} and is the
+ * primary access point for service operations in application layer code running
+ * on a remote server. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely.
+ *
+ * @author Brian Wing Shun Chan
+ * @see FooService
+ * @see blade.servicebuilder.service.base.FooServiceBaseImpl
+ * @see blade.servicebuilder.service.impl.FooServiceImpl
+ * @generated
+ */
+@ProviderType
+public class FooServiceUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to {@link blade.servicebuilder.service.impl.FooServiceImpl} and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	public static java.lang.String getBeanIdentifier() {
+		return getService().getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	public static void setBeanIdentifier(java.lang.String beanIdentifier) {
+		getService().setBeanIdentifier(beanIdentifier);
+	}
+
+	public static FooService getService() {
+		if (_service == null) {
+			_service = (FooService)PortalBeanLocatorUtil.locate(FooService.class.getName());
+
+			ReferenceRegistry.registerReference(FooServiceUtil.class, "_service");
+		}
+
+		return _service;
+	}
+
+	/**
+	 * @deprecated As of 6.2.0
+	 */
+	@Deprecated
+	public void setService(FooService service) {
+	}
+
+	private static FooService _service;
+}
