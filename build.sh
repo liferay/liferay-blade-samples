568959d448236cf9029f37fa93499a35e36c3df3
Raymond Auge <raymond.auge@liferay.com>
2015-06-10 14:10:40 -0400

    basic build

--- build.sh
+++ build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "Hello World!"
+#for dir in $( ls -d bndtools/*/ ); do
+#	pushd $dir
+#	./gradlew build
+#	popd
+#done
+
+for dir in $( ls -d gradle/*/ ); do
+	echo "Building [$dir]"
+	pushd $dir
+	./gradlew build
+	popd
+done
+
+for dir in $( ls -d maven/*/ ); do
+	echo "Building [$dir]"
+	pushd $dir
+	mvn package
+	popd
+done
