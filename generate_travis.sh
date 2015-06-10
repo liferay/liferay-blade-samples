f4719c8cc26023ae1882b8d62fd46d9a53f8ad59
Raymond Auge <raymond.auge@liferay.com>
2015-06-10 17:41:13 -0400

    cd to gradle projects

--- generate_travis.sh
+++ generate_travis.sh
@@ -11,7 +11,7 @@ echo ""
 echo "script:"
 
 for dir in $( ls -d gradle/*/ ); do
-	echo "    - ${dir}gradlew build"
+	echo "    - cd ${dir}; ./gradlew build"
 done
 
 echo "    - cd maven; mvn --fail-at-end package"
