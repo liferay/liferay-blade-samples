b3a95a5c9b1812498f5165db2208d8552a3f7485
Andy Wu <andy.wu@liferay.com>
2015-10-30 16:53:16 +0800

    [bndtools] servicebuilder.web bundle consume OSGi Services

--- bndtools/blade.servicebuilder.svc/src/blade/servicebuilder/service/base/FooServiceBaseImpl.java
+++ bndtools/blade.servicebuilder.svc/src/blade/servicebuilder/service/base/FooServiceBaseImpl.java
@@ -20,12 +20,12 @@ import blade.servicebuilder.service.FooService;
 import blade.servicebuilder.service.persistence.FooPersistence;
 
 import com.liferay.portal.kernel.bean.BeanReference;
-import com.liferay.portal.kernel.bean.IdentifiableBean;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
 import com.liferay.portal.kernel.dao.jdbc.SqlUpdate;
 import com.liferay.portal.kernel.dao.jdbc.SqlUpdateFactoryUtil;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.module.framework.service.IdentifiableOSGiService;
 import com.liferay.portal.service.BaseServiceImpl;
 import com.liferay.portal.service.persistence.ClassNamePersistence;
 import com.liferay.portal.service.persistence.UserPersistence;
@@ -49,7 +49,7 @@ import javax.sql.DataSource;
  * @generated
  */
 public abstract class FooServiceBaseImpl extends BaseServiceImpl
-	implements FooService, IdentifiableBean {
+	implements FooService, IdentifiableOSGiService {
 	/*
 	 * NOTE FOR DEVELOPERS:
 	 *
@@ -382,23 +382,13 @@ public abstract class FooServiceBaseImpl extends BaseServiceImpl
 	}
 
 	/**
-	 * Returns the Spring bean ID for this bean.
+	 * Returns the OSGi service identifier.
 	 *
-	 * @return the Spring bean ID for this bean
+	 * @return the OSGi service identifier
 	 */
 	@Override
-	public String getBeanIdentifier() {
-		return _beanIdentifier;
-	}
-
-	/**
-	 * Sets the Spring bean ID for this bean.
-	 *
-	 * @param beanIdentifier the Spring bean ID for this bean
-	 */
-	@Override
-	public void setBeanIdentifier(String beanIdentifier) {
-		_beanIdentifier = beanIdentifier;
+	public String getOSGiServiceIdentifier() {
+		return FooService.class.getName();
 	}
 
 	protected Class<?> getModelClass() {
@@ -467,5 +457,4 @@ public abstract class FooServiceBaseImpl extends BaseServiceImpl
 	protected com.liferay.portlet.asset.service.AssetTagService assetTagService;
 	@BeanReference(type = AssetTagPersistence.class)
 	protected AssetTagPersistence assetTagPersistence;
-	private String _beanIdentifier;
 }
