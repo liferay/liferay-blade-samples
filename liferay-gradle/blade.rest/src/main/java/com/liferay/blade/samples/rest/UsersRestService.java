45eeea42605e4387ee490d54a42e307bb88e0cbd
Gregory Amerson <gregory.amerson@liferay.com>
2016-04-12 13:20:25 -0500

    blade.rest apply SF

--- liferay-gradle/blade.rest/src/main/java/com/liferay/blade/samples/rest/UsersRestService.java
+++ liferay-gradle/blade.rest/src/main/java/com/liferay/blade/samples/rest/UsersRestService.java
@@ -1,5 +1,23 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-package blade.rest;
+package com.liferay.blade.samples.rest;
+
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
 
 import java.util.Collections;
 import java.util.Set;
@@ -12,21 +30,18 @@ import javax.ws.rs.core.Application;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.UserLocalService;
-
+/**
+ * @author Liferay
+ */
 @Component(
-	immediate = true,
-	property = {
-		"jaxrs.application=true"
-	},
-	service = Application.class)
+	immediate = true, property = {"jaxrs.application=true"},
+	service = Application.class
+)
 @Path("/blade.users")
 public class UsersRestService extends Application {
 
 	@Override
 	public Set<Object> getSingletons() {
-
 		return Collections.<Object> singleton(this);
 	}
 
@@ -34,17 +49,17 @@ public class UsersRestService extends Application {
 	@Path("/list")
 	@Produces("text/plain")
 	public String getUsers() {
-
 		StringBuilder result = new StringBuilder();
+
 		for (User user : _userLocalService.getUsers(-1, -1)) {
 			result.append(user.getFullName()).append(",\n");
 		}
+
 		return result.toString();
 	}
 
 	@Reference
 	public void setUserLocalService(UserLocalService userLocalService) {
-
 		this._userLocalService = userLocalService;
 	}
 
