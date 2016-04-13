a8048537d068fbf673affc33486c681794c62f67
Gregory Amerson <gregory.amerson@liferay.com>
2016-04-13 17:49:36 -0500

    [liferay-gradle] formatSource

--- liferay-gradle/blade.resourcebundle/src/main/java/com/liferay/blade/samples/resourcebundle/CustomResourceBundle.java
+++ liferay-gradle/blade.resourcebundle/src/main/java/com/liferay/blade/samples/resourcebundle/CustomResourceBundle.java
@@ -1,12 +1,31 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.samples.resourcebundle;
+
+import com.liferay.portal.kernel.language.UTF8Control;
 
 import java.util.Enumeration;
 import java.util.ResourceBundle;
 
-import org.osgi.service.component.annotations.*;
+import org.osgi.service.component.annotations.Component;
 
-import com.liferay.portal.kernel.language.UTF8Control;
-
+/**
+ * @author Liferay
+ */
 @Component(
 	immediate = true, property = {"language.id=en_US"},
 	service = ResourceBundle.class
@@ -14,13 +33,13 @@ import com.liferay.portal.kernel.language.UTF8Control;
 public class CustomResourceBundle extends ResourceBundle {
 
 	@Override
-	protected Object handleGetObject(String key) {
-		return _resourceBundle.getObject(key);
+	public Enumeration<String> getKeys() {
+		return _resourceBundle.getKeys();
 	}
 
 	@Override
-	public Enumeration<String> getKeys() {
-		return _resourceBundle.getKeys();
+	protected Object handleGetObject(String key) {
+		return _resourceBundle.getObject(key);
 	}
 
 	private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
