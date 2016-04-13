9ac0e2d063d57dd3e0c9d84764329f6aaafd4d85
Gregory Amerson <gregory.amerson@liferay.com>
2016-04-13 12:35:10 -0500

    local build script

---
+++ build-local.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+repoDir=`pwd`
+function buildAll() {
+  cd "$repoDir"
+  cd bndtools
+  ./gradlew clean build
+  cd "$repoDir"
+  cd gradle
+  ./gradlew clean build
+  cd "$repoDir"
+  cd liferay-gradle
+  ./gradlew clean build
+  cd "$repoDir"
+  cd maven
+  mvn --fail-at-end clean package
+  cd "$repoDir"
+  ./gradlew outputFilesTest
+}
+buildAll
