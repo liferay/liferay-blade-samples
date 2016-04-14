e2a8ae5fdbee08a90d2cb47cfda03cd4642be8ac
Gregory Amerson <gregory.amerson@liferay.com>
2016-04-13 19:50:07 -0500

    [liferay-gradle] add blade.corejsphook sample

---
+++ liferay-gradle/blade.corejsphook/src/main/java/com/liferay/blade/samples/corejsphook/BladeCustomJspBag.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.blade.samples.corejsphook;
+
+import com.liferay.portal.deploy.hot.CustomJspBag;
+import com.liferay.portal.kernel.url.URLContainer;
+
+import java.net.URL;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		"context.id=BladeCustomJspBag", "context.name=Test Custom JSP Bag",
+		"service.ranking:Integer=100"
+	}
+)
+public class BladeCustomJspBag implements CustomJspBag {
+
+	@Override
+	public String getCustomJspDir() {
+		return "META-INF/jsps/";
+	}
+
+	@Override
+	public List<String> getCustomJsps() {
+		return _customJsps;
+	}
+
+	@Override
+	public URLContainer getURLContainer() {
+		return _urlContainer;
+	}
+
+	@Override
+	public boolean isCustomJspGlobal() {
+		return true;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		bundle = bundleContext.getBundle();
+
+		_customJsps = new ArrayList<>();
+
+		Enumeration<URL> entries = bundle.findEntries(
+			getCustomJspDir(), "*.jsp", true);
+
+		while (entries.hasMoreElements()) {
+			URL url = entries.nextElement();
+
+			_customJsps.add(url.getPath());
+		}
+	}
+
+	private List<String> _customJsps;
+
+	private final URLContainer _urlContainer = new URLContainer() {
+
+		@Override
+		public Set<String> getResources(String path) {
+			Set<String> paths = new HashSet<>();
+
+			for (String entry : _customJsps) {
+				if (entry.startsWith(path)) {
+					paths.add(entry);
+				}
+			}
+
+			return paths;
+		}
+
+		@Override
+		public URL getResource(String name) {
+			return bundle.getEntry(name);
+		}
+
+	};
+
+	private Bundle bundle;
+
+}
