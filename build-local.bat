5b117a10c13f79f20a609bf677543f6ac79b1061
Lawrence Lee <lawrence.lee@liferay.com>
2016-10-11 11:58:17 -0500

    Cleanup script

--- build-local.bat
+++ build-local.bat
@@ -1,19 +1,18 @@
-repoDir=`echo %cd%`
+set repoDir=%cd%
 
-call cd "$repoDir"
 call cd bndtools
 call gradlew.bat clean build
-call cd "$repoDir"
+call cd %repoDir%
 call cd gradle
 call gradlew.bat clean build
-call cd "$repoDir"
+call cd %repoDir%
 call cd liferay-gradle
 call gradlew.bat clean build
-call cd "$repoDir"
+call cd %repoDir%
 call cd maven
 call mvn --fail-at-end clean package
-call cd "$repoDir"
+call cd %repoDir%
 call cd liferay-workspace
 call gradlew.bat clean build -x :tests:test
-call cd "$repoDir"
+call cd %repoDir%
 call gradlew.bat outputFilesTest diff
