445768f107bdaa634b81ff3b11cea93acacbbc95
Romeo Sheshi <rsheshi@gmail.com>
2016-11-20 21:07:10 +0100

    fix service ranking and add configuration
    Add  service ranking for make liferay load the custom validator and add
    configuration

---
+++ liferay-workspace/modules/blade.screenname.validator/src/main/java/com/liferay/blade/samples/screenname/constants/CustomScreenName.java
@@ -0,0 +1,11 @@
+package com.liferay.blade.samples.screenname.constants;
+
+/**
+ * User: Romeo Sheshi <a href="mailto:romeo.sheshi@sourcesence.com">Romeo Sheshi</a>
+ * Date: 20/11/2016
+ * Time: 13:59
+ */
+public interface CustomScreenName {
+
+    String SERVICE_NAME = "CustomScreenNameConfiguration";
+}
