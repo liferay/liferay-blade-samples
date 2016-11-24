6d11c7c4d80b4e50bcad92f72c951eafae455148
Romeo Sheshi <rsheshi@gmail.com>
2016-11-24 11:12:27 +0100

    rebase with liferay/master and docs
    rebase with the last version of liferay/master, adding  README.md and
    java docs

--- liferay-workspace/modules/blade.servlet.filter/src/main/java/com/liferay/blade/samples/servlet/filter/ServletFilterExample.java
+++ liferay-workspace/modules/blade.servlet.filter/src/main/java/com/liferay/blade/samples/servlet/filter/ServletFilterExample.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
+ * The plugin is an example of servlet filter. It will log the request and the response at debug level.
  * User: Romeo Sheshi <a href="mailto:rsheshi@gmail.com">Romeo Sheshi</a>
  * Date: 23/11/2016
  * Time: 14:14
@@ -26,13 +27,25 @@ import java.util.Map;
 public class ServletFilterExample implements Filter {
     private static Log _log = LogFactoryUtil.getLog(ServletFilterExample.class);
 
-
+    /**
+     * Called by the web container when instacate the filter,
+     * it's possibile to configurate filter global information.
+     * @param filterConfig
+     * @throws ServletException
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
-
+    /**
+     * Check if the log is in debug mode than log information about the request and response.
+     * @param request {@link javax.servlet.ServletRequest}
+     * @param response {@link javax.servlet.ServletResponse}
+     * @param chain {@link javax.servlet.FilterChain}
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         if(_log.isDebugEnabled()) {
@@ -44,6 +57,12 @@ public class ServletFilterExample implements Filter {
         }
     }
 
+    /**
+     * Log in debug the response information
+     * [Response log][Status :200][Content type :{@link javax.servlet.http.HttpServletResponse#getContentType()}]
+     * [Char encoding :{@link javax.servlet.http.HttpServletResponse#getCharacterEncoding()}].
+     * @param response {@link javax.servlet.http.HttpServletResponse}
+     */
     private void logResponse(HttpServletResponse response) {
         StringBuilder logMessage = new StringBuilder();
         logMessage.append("[Response log]");
@@ -59,7 +78,11 @@ public class ServletFilterExample implements Filter {
         _log.debug(logMessage.toString());
 
     }
-
+    /**
+     * Log in debug the request information
+     * Request log][path :{@link javax.servlet.http.HttpServletRequest#getPathInfo()}][parameters : [{@link javax.servlet.http.HttpServletRequest#getParameterMap()}]]
+     * @param request {@link javax.servlet.http.HttpServletRequest}
+     */
     private void logRequest(HttpServletRequest request) {
         Map<String, String[]> parameterMap = request.getParameterMap();
         StringBuilder logMessage = new StringBuilder();
@@ -86,7 +109,9 @@ public class ServletFilterExample implements Filter {
         _log.debug(logMessage.toString());
     }
 
-
+    /**
+     * Called when the filter going to be destroyed.
+     */
     @Override
     public void destroy() {
 
