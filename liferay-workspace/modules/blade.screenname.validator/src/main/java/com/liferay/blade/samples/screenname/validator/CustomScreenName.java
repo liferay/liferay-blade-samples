9535e5ebfa0a1f1cb0a877179a0c495682627e8b
Romeo Sheshi <rsheshi@gmail.com>
2016-11-24 11:12:27 +0100

    rebase with liferay/master and docs
    rebase with the last version of liferay/master, adding  README.md and
    java docs

--- liferay-workspace/modules/blade.screenname.validator/src/main/java/com/liferay/blade/samples/screenname/validator/CustomScreenName.java
+++ liferay-workspace/modules/blade.screenname.validator/src/main/java/com/liferay/blade/samples/screenname/validator/CustomScreenName.java
@@ -4,7 +4,9 @@ package com.liferay.blade.samples.screenname.validator;
  * @author Romeo Sheshi
  */
 public interface CustomScreenName {
-
+	/**
+	 * Setting id mapped in the factory configuration.
+	 */
 	public String SETTINGS_ID = "CustomScreenNameConfiguration";
 
 }
