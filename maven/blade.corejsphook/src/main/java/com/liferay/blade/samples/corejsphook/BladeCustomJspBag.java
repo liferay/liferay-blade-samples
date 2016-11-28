b7c31deb5f10b123bec3478ef027055541551eca
Gregory Amerson <gregory.amerson@liferay.com>
2016-11-28 08:23:44 -0600

    replicate sources

--- maven/blade.corejsphook/src/main/java/com/liferay/blade/samples/corejsphook/BladeCustomJspBag.java
+++ maven/blade.corejsphook/src/main/java/com/liferay/blade/samples/corejsphook/BladeCustomJspBag.java
@@ -33,22 +33,53 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 /**
+ * Represents a custom JSP bag used to override Liferay's core JSPs. This class
+ * sets the following properties:
+ *
+ * <ul>
+ * <li>
+ * <code>context.id</code>: the custom JSP bag class name
+ * </li>
+ * <li>
+ * <code>context.name</code>: the human readable name for you service
+ * </li>
+ * <li>
+ * <code>service.ranking</code>: the priority of your implementation. The
+ * importance of your change dictates its service ranking. For example, if
+ * another custom JSP bag implementation overriding the same JSP is deployed
+ * with a service rank of <code>101</code>, it takes precedence over this one
+ * because its service ranking is higher.
+ * </li>
+ * </ul>
+ *
  * @author Liferay
  */
 @Component(
 	immediate = true,
 	property = {
-		"context.id=BladeCustomJspBag", "context.name=Test Custom JSP Bag",
+		"context.id=BladeCustomJspBag",
+		"context.name=Test Custom JSP Bag",
 		"service.ranking:Integer=100"
 	}
 )
 public class BladeCustomJspBag implements CustomJspBag {
 
+	/**
+	 * Returns the directory path in the module JAR where the custom JSPs
+	 * reside.
+	 *
+	 * @return the directory path in the module JAR where the custom JSPs reside
+	 */
 	@Override
 	public String getCustomJspDir() {
 		return "META-INF/jsps/";
 	}
 
+	/**
+	 * Returns the custom JSP URL paths.
+	 *
+	 * @return the custom JSP URL paths
+	 */
 	@Override
 	public List<String> getCustomJsps() {
 		return _customJsps;
@@ -64,6 +95,13 @@ public class BladeCustomJspBag implements CustomJspBag {
 		return true;
 	}
 
+	/**
+	 * Adds the URL paths for all custom core JSPs to a list when the module is
+	 * activated.
+	 *
+	 * @param bundleContext the bundle context from which to get the custom
+	 *        JSP's bundle
+	 */
 	@Activate
 	protected void activate(BundleContext bundleContext) {
 		_bundle = bundleContext.getBundle();
