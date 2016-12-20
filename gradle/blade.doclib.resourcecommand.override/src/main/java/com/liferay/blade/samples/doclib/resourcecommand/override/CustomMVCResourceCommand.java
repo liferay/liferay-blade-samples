d5b2bb3155fe57251aa880dfcfb67af32ff7766f
Gregory Amerson <gregory.amerson@liferay.com>
2016-12-20 09:14:05 -0600

    replicate sources

--- gradle/blade.doclib.resourcecommand.override/src/main/java/com/liferay/blade/samples/doclib/resourcecommand/override/CustomMVCResourceCommand.java
+++ gradle/blade.doclib.resourcecommand.override/src/main/java/com/liferay/blade/samples/doclib/resourcecommand/override/CustomMVCResourceCommand.java
@@ -17,7 +17,6 @@
 package com.liferay.blade.samples.doclib.resourcecommand.override;
 
 import com.liferay.document.library.web.constants.DLPortletKeys;
-
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -30,47 +29,44 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
-* @author Michael C. Han
-*/
+ * @author Liferay
+ */
 @Component(
-   property = {
-      "javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
-      "javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
-      "javax.portlet.name=" + DLPortletKeys.MEDIA_GALLERY_DISPLAY,
-      "mvc.command.name=/document_library/edit_folder",
-      "service.rank=100"
-   },
-   service = MVCResourceCommand.class
+	property = {
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"javax.portlet.name=" + DLPortletKeys.MEDIA_GALLERY_DISPLAY,
+		"mvc.command.name=/document_library/edit_folder", "service.rank=100"
+	},
+	service = MVCResourceCommand.class
 )
 public class CustomMVCResourceCommand implements MVCResourceCommand {
 
-   @Override
-   public boolean serveResource(
-         ResourceRequest resourceRequest, ResourceResponse resourceResponse)
-      throws PortletException {
+	@Override
+	public boolean serveResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws PortletException {
 
-      final String CMD = ParamUtil.getString(resourceRequest, Constants.CMD);
+		final String cmd = ParamUtil.getString(resourceRequest, Constants.CMD);
 
-      if(CMD.equals("BOOKMARK_ORDERABLE")){
-         serveOlderUserBookmarks(resourceRequest, resourceResponse);
-      }
-      else {
-         _editFolderMVCResourceCommand.serveResource(
-            resourceRequest, resourceResponse);
-      }
+		if (cmd.equals("BOOKMARK_ORDERABLE")) {
+			serveOlderUserBookmarks(resourceRequest, resourceResponse);
+		} else {
+			_editFolderMVCResourceCommand.serveResource(
+				resourceRequest, resourceResponse);
+		}
 
-      return true;
-   }
+		return true;
+	}
 
-   protected void serveOlderUserBookmarks(
-      ResourceRequest resourceRequest, ResourceResponse resourceResponse) {
+	protected void serveOlderUserBookmarks(
+		ResourceRequest resourceRequest, ResourceResponse resourceResponse) {
 
-      //do your custom logic
-   }
+		// do your custom logic
 
-   @Reference(
-      target = "(component.name=com.liferay.document.library.web.portlet.action.EditFolderMVCResourceCommand)"
-   )
-   private MVCResourceCommand _editFolderMVCResourceCommand;
+	}
+
+	@Reference(target = "(component.name=com.liferay.document.library.web.portlet.action.EditFolderMVCResourceCommand)")
+	private MVCResourceCommand _editFolderMVCResourceCommand;
 
 }
