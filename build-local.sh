a1c8674d317dacb2f6c3a5100f3275586a9d418c
Lawrence Lee <lawrence.lee@liferay.com>
2017-07-13 14:52:30 -0500

    Auto SF

---
+++ build-local.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -xe
+repoDir=`pwd`
+function buildAll() {
+  cd "$repoDir"
+  cd gradle
+  ./gradlew clean build
+  cd "$repoDir"
+  cd maven
+  mvn --fail-at-end clean package
+  cd "$repoDir"
+  cd liferay-workspace
+  ./gradlew clean build -x testIntegration
+  cd "$repoDir"
+  ./gradlew bundlesTest warsTest diff
+  cd liferay-workspace
+  ./gradlew check -Pliferay.workspace.modules.dir=modules,tests -Pliferay.workspace.modules.jsp.precompile.enabled=true $@
+}
+buildAll
--- build.sh
+++ build.sh
@@ -2,18 +2,7 @@
 set -xe
 repoDir=`pwd`
 function buildAll() {
-  cd "$repoDir"
-  cd gradle
-  ./gradlew clean build
-  cd "$repoDir"
-  cd maven
-  mvn --fail-at-end clean package
-  cd "$repoDir"
   cd liferay-workspace
-  ./gradlew clean build -x testIntegration
-  cd "$repoDir"
-  ./gradlew bundlesTest warsTest diff
-  cd liferay-workspace
-  ./gradlew check -Pliferay.workspace.modules.dir=modules,tests -Pliferay.workspace.modules.jsp.precompile.enabled=true $@
+  ./gradlew check -x testIntegration -Pliferay.workspace.modules.dir=modules,tests -Pliferay.workspace.modules.jsp.precompile.enabled=true $@
 }
 buildAll
