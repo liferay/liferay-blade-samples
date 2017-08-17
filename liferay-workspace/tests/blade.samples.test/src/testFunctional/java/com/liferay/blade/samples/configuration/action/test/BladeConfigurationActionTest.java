54159d3022092e98128858ed1c97f15641395af7
Lawrence Lee <lawrence.lee@liferay.com>
2017-08-17 15:33:33 -0500

    Add Configuration Action Functional Test

---
+++ liferay-workspace/tests/blade.samples.test/src/testFunctional/java/com/liferay/blade/samples/configuration/action/test/BladeConfigurationActionTest.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.blade.samples.configuration.action.test;
+
+import com.liferay.arquillian.portal.annotation.PortalURL;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.io.File;
+
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * @author Lawrence Lee
+ */
+@RunAsClient
+@RunWith(Arquillian.class)
+public class BladeConfigurationActionTest {
+
+	@Deployment
+	public static JavaArchive create() throws Exception {
+		final File jarFile = new File(
+			System.getProperty("configurationActionJarFile"));
+
+		return ShrinkWrap.createFromZipFile(JavaArchive.class, jarFile);
+	}
+
+	public void customClick(WebDriver webDriver, WebElement webElement) {
+		Actions action = new Actions(webDriver);
+
+		action.moveToElement(webElement).build().perform();
+
+		WebDriverWait wait = new WebDriverWait(webDriver, 30);
+
+		WebElement element = wait.until(
+			ExpectedConditions.elementToBeClickable(webElement));
+
+		element.click();
+	}
+
+
+	@Test
+	public void testBladeConfigurationAction()
+		throws InterruptedException, PortalException {
+
+		_webDriver.get(_portletURL.toExternalForm());
+
+		Assert.assertTrue(
+			"Portlet was not deployed", isVisible(_bladeMessagePortlet));
+
+		_bodyWebElement.click();
+
+		customClick(_webDriver, _verticalEllipsis);
+
+		customClick(_webDriver, _lfrMenuConfiguration);
+
+		customClick(_webDriver, _saveButton);
+
+		Assert.assertTrue(
+				"Expected Blade Message Portlet, but saw: " + _portletTitle.getText(),
+				_portletTitle.getText().contentEquals("Blade Message Portlet"));
+
+		Assert.assertTrue("Expected Hello from BLADE JSP!, but saw: " + _portletBody.getText(),
+				_portletBody.getText().contentEquals("Hello from BLADE JSP!"));
+
+	}
+
+	protected boolean isVisible(WebElement webelement) {
+		WebDriverWait webDriverWait = new WebDriverWait(_webDriver, 15);
+
+		try {
+			webDriverWait.until(ExpectedConditions.visibilityOf(webelement));
+
+			return true;
+		}
+		catch (org.openqa.selenium.TimeoutException te) {
+			return false;
+		}
+	}
+
+	@FindBy(xpath = "//body")
+	private WebElement _bodyWebElement;
+
+	@FindBy(xpath = "//section[contains(@id,'BladeMessagePortlet')]")
+	private WebElement _bladeMessagePortlet;;
+
+	@FindBy(xpath = "//ul[contains(@class,'dropdown-menu')]/li[contains(@class,'configuration')]")
+	private WebElement _lfrMenuConfiguration;
+
+	@FindBy(xpath = "//div[contains(@id,'BladeMessagePortlet')]//..//p")
+	private WebElement _portletBody;
+
+	@FindBy(xpath = "//div[contains(@id,'BladeMessagePortlet')]//..//h2")
+	private WebElement _portletTitle;
+
+	@PortalURL("BladeMessagePortlet")
+	private URL _portletURL;
+
+	@FindBy(css = "button[type=submit]")
+	private WebElement _saveButton;
+
+	@FindBy(xpath = "//section[contains(@id,'BladeMessagePortlet')]//..//span/*[name()='svg'][contains(@class,'icon-ellipsis')]")
+	private WebElement _verticalEllipsis;
+
+	@Drone
+	private WebDriver _webDriver;
+
+}
