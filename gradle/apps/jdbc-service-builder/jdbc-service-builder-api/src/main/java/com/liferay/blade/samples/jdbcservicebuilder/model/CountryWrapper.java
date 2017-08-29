053fe55031f1ef9861cc50e43f0e4b0fea2a3b33
Jesse Rao <jesse.rao@liferay.com>
2017-08-29 16:36:17 -0400

    BLADE-178, 179 Add Blade samples demonstrating how to use an external database via a JDBC or JNDI connection from Service Builder; add READMEs with explanation

---
+++ gradle/apps/jdbc-service-builder/jdbc-service-builder-api/src/main/java/com/liferay/blade/samples/jdbcservicebuilder/model/CountryWrapper.java
@@ -0,0 +1,281 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blade.samples.jdbcservicebuilder.model;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.expando.kernel.model.ExpandoBridge;
+
+import com.liferay.portal.kernel.model.ModelWrapper;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.io.Serializable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * <p>
+ * This class is a wrapper for {@link Country}.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see Country
+ * @generated
+ */
+@ProviderType
+public class CountryWrapper implements Country, ModelWrapper<Country> {
+	public CountryWrapper(Country country) {
+		_country = country;
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return Country.class;
+	}
+
+	@Override
+	public String getModelClassName() {
+		return Country.class.getName();
+	}
+
+	@Override
+	public Map<String, Object> getModelAttributes() {
+		Map<String, Object> attributes = new HashMap<String, Object>();
+
+		attributes.put("countryId", getCountryId());
+		attributes.put("countryName", getCountryName());
+
+		return attributes;
+	}
+
+	@Override
+	public void setModelAttributes(Map<String, Object> attributes) {
+		Long countryId = (Long)attributes.get("countryId");
+
+		if (countryId != null) {
+			setCountryId(countryId);
+		}
+
+		String countryName = (String)attributes.get("countryName");
+
+		if (countryName != null) {
+			setCountryName(countryName);
+		}
+	}
+
+	@Override
+	public java.lang.Object clone() {
+		return new CountryWrapper((Country)_country.clone());
+	}
+
+	@Override
+	public int compareTo(Country country) {
+		return _country.compareTo(country);
+	}
+
+	/**
+	* Returns the country ID of this country.
+	*
+	* @return the country ID of this country
+	*/
+	@Override
+	public long getCountryId() {
+		return _country.getCountryId();
+	}
+
+	/**
+	* Returns the country name of this country.
+	*
+	* @return the country name of this country
+	*/
+	@Override
+	public java.lang.String getCountryName() {
+		return _country.getCountryName();
+	}
+
+	@Override
+	public ExpandoBridge getExpandoBridge() {
+		return _country.getExpandoBridge();
+	}
+
+	/**
+	* Returns the primary key of this country.
+	*
+	* @return the primary key of this country
+	*/
+	@Override
+	public long getPrimaryKey() {
+		return _country.getPrimaryKey();
+	}
+
+	@Override
+	public Serializable getPrimaryKeyObj() {
+		return _country.getPrimaryKeyObj();
+	}
+
+	@Override
+	public int hashCode() {
+		return _country.hashCode();
+	}
+
+	@Override
+	public boolean isCachedModel() {
+		return _country.isCachedModel();
+	}
+
+	@Override
+	public boolean isEscapedModel() {
+		return _country.isEscapedModel();
+	}
+
+	@Override
+	public boolean isNew() {
+		return _country.isNew();
+	}
+
+	@Override
+	public void persist() {
+		_country.persist();
+	}
+
+	@Override
+	public void setCachedModel(boolean cachedModel) {
+		_country.setCachedModel(cachedModel);
+	}
+
+	/**
+	* Sets the country ID of this country.
+	*
+	* @param countryId the country ID of this country
+	*/
+	@Override
+	public void setCountryId(long countryId) {
+		_country.setCountryId(countryId);
+	}
+
+	/**
+	* Sets the country name of this country.
+	*
+	* @param countryName the country name of this country
+	*/
+	@Override
+	public void setCountryName(java.lang.String countryName) {
+		_country.setCountryName(countryName);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(
+		com.liferay.portal.kernel.model.BaseModel<?> baseModel) {
+		_country.setExpandoBridgeAttributes(baseModel);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(ExpandoBridge expandoBridge) {
+		_country.setExpandoBridgeAttributes(expandoBridge);
+	}
+
+	@Override
+	public void setExpandoBridgeAttributes(ServiceContext serviceContext) {
+		_country.setExpandoBridgeAttributes(serviceContext);
+	}
+
+	@Override
+	public void setNew(boolean n) {
+		_country.setNew(n);
+	}
+
+	/**
+	* Sets the primary key of this country.
+	*
+	* @param primaryKey the primary key of this country
+	*/
+	@Override
+	public void setPrimaryKey(long primaryKey) {
+		_country.setPrimaryKey(primaryKey);
+	}
+
+	@Override
+	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
+		_country.setPrimaryKeyObj(primaryKeyObj);
+	}
+
+	@Override
+	public com.liferay.portal.kernel.model.CacheModel<Country> toCacheModel() {
+		return _country.toCacheModel();
+	}
+
+	@Override
+	public Country toEscapedModel() {
+		return new CountryWrapper(_country.toEscapedModel());
+	}
+
+	@Override
+	public java.lang.String toString() {
+		return _country.toString();
+	}
+
+	@Override
+	public Country toUnescapedModel() {
+		return new CountryWrapper(_country.toUnescapedModel());
+	}
+
+	@Override
+	public java.lang.String toXmlString() {
+		return _country.toXmlString();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof CountryWrapper)) {
+			return false;
+		}
+
+		CountryWrapper countryWrapper = (CountryWrapper)obj;
+
+		if (Objects.equals(_country, countryWrapper._country)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public Country getWrappedModel() {
+		return _country;
+	}
+
+	@Override
+	public boolean isEntityCacheEnabled() {
+		return _country.isEntityCacheEnabled();
+	}
+
+	@Override
+	public boolean isFinderCacheEnabled() {
+		return _country.isFinderCacheEnabled();
+	}
+
+	@Override
+	public void resetOriginalValues() {
+		_country.resetOriginalValues();
+	}
+
+	private final Country _country;
+}
---
+++ gradle/apps/jdbc-service-builder/jdbc-service-builder-api/src/main/java/com/liferay/blade/samples/jdbcservicebuilder/service/persistence/CountryPersistence.java
@@ -0,0 +1,165 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blade.samples.jdbcservicebuilder.service.persistence;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.blade.samples.jdbcservicebuilder.exception.NoSuchCountryException;
+import com.liferay.blade.samples.jdbcservicebuilder.model.Country;
+
+import com.liferay.portal.kernel.service.persistence.BasePersistence;
+
+/**
+ * The persistence interface for the country service.
+ *
+ * <p>
+ * Caching information and settings can be found in <code>portal.properties</code>
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see com.liferay.blade.samples.jdbcservicebuilder.service.persistence.impl.CountryPersistenceImpl
+ * @see CountryUtil
+ * @generated
+ */
+@ProviderType
+public interface CountryPersistence extends BasePersistence<Country> {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link CountryUtil} to access the country persistence. Modify <code>service.xml</code> and rerun ServiceBuilder to regenerate this interface.
+	 */
+
+	/**
+	* Caches the country in the entity cache if it is enabled.
+	*
+	* @param country the country
+	*/
+	public void cacheResult(Country country);
+
+	/**
+	* Caches the countries in the entity cache if it is enabled.
+	*
+	* @param countries the countries
+	*/
+	public void cacheResult(java.util.List<Country> countries);
+
+	/**
+	* Creates a new country with the primary key. Does not add the country to the database.
+	*
+	* @param countryId the primary key for the new country
+	* @return the new country
+	*/
+	public Country create(long countryId);
+
+	/**
+	* Removes the country with the primary key from the database. Also notifies the appropriate model listeners.
+	*
+	* @param countryId the primary key of the country
+	* @return the country that was removed
+	* @throws NoSuchCountryException if a country with the primary key could not be found
+	*/
+	public Country remove(long countryId) throws NoSuchCountryException;
+
+	public Country updateImpl(Country country);
+
+	/**
+	* Returns the country with the primary key or throws a {@link NoSuchCountryException} if it could not be found.
+	*
+	* @param countryId the primary key of the country
+	* @return the country
+	* @throws NoSuchCountryException if a country with the primary key could not be found
+	*/
+	public Country findByPrimaryKey(long countryId)
+		throws NoSuchCountryException;
+
+	/**
+	* Returns the country with the primary key or returns <code>null</code> if it could not be found.
+	*
+	* @param countryId the primary key of the country
+	* @return the country, or <code>null</code> if a country with the primary key could not be found
+	*/
+	public Country fetchByPrimaryKey(long countryId);
+
+	@Override
+	public java.util.Map<java.io.Serializable, Country> fetchByPrimaryKeys(
+		java.util.Set<java.io.Serializable> primaryKeys);
+
+	/**
+	* Returns all the countries.
+	*
+	* @return the countries
+	*/
+	public java.util.List<Country> findAll();
+
+	/**
+	* Returns a range of all the countries.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link CountryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param start the lower bound of the range of countries
+	* @param end the upper bound of the range of countries (not inclusive)
+	* @return the range of countries
+	*/
+	public java.util.List<Country> findAll(int start, int end);
+
+	/**
+	* Returns an ordered range of all the countries.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link CountryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param start the lower bound of the range of countries
+	* @param end the upper bound of the range of countries (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the ordered range of countries
+	*/
+	public java.util.List<Country> findAll(int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<Country> orderByComparator);
+
+	/**
+	* Returns an ordered range of all the countries.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link CountryModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param start the lower bound of the range of countries
+	* @param end the upper bound of the range of countries (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @param retrieveFromCache whether to retrieve from the finder cache
+	* @return the ordered range of countries
+	*/
+	public java.util.List<Country> findAll(int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<Country> orderByComparator,
+		boolean retrieveFromCache);
+
+	/**
+	* Removes all the countries from the database.
+	*/
+	public void removeAll();
+
+	/**
+	* Returns the number of countries.
+	*
+	* @return the number of countries
+	*/
+	public int countAll();
+
+	@Override
+	public java.util.Set<java.lang.String> getBadColumnNames();
+}
