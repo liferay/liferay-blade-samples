f2117ce4e94120b4287b50ef0e0e49a00b61dcd2
Lawrence Lee <lawrence.lee@liferay.com>
2017-09-07 16:51:42 -0500

    update SF

--- liferay-workspace/extensions/document-action/src/main/java/blade/document/action/portlet/BladeDocumentActionPortlet.java
+++ liferay-workspace/extensions/document-action/src/main/java/blade/document/action/portlet/BladeDocumentActionPortlet.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package blade.document.action.portlet;
 
 import java.io.IOException;
