6a13faf2ebd2233f4ce4956895fa2eeec1821d1e
Lawrence Lee <lawrence.lee@liferay.com>
2017-11-07 14:00:06 -0600

    Fix SF failures

--- gradle/extensions/http-servlet/src/main/java/com/liferay/blade/samples/httpservlet/BladeHttpServlet.java
+++ gradle/extensions/http-servlet/src/main/java/com/liferay/blade/samples/httpservlet/BladeHttpServlet.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.samples.httpservlet;
 
 import com.liferay.portal.kernel.log.Log;
@@ -26,51 +42,12 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"osgi.http.whiteboard.context.path=/",
-		"osgi.http.whiteboard.servlet.pattern=/blade/httpservlet/*",
+		"osgi.http.whiteboard.servlet.pattern=/blade/httpservlet/*"
 	},
 	service = Servlet.class
 )
 public class BladeHttpServlet extends HttpServlet {
 
-	/**
-	 * Dummy contents
-	 * 
-	 * @return dummy contents string
-	 */
-	protected String generateSampleHTML() {
-		StringBuffer sb = new StringBuffer();
-
-		sb.append("<html>");
-		sb.append("<head><title>Sample HTML</title></head>");
-		sb.append("<body>");
-		sb.append("<h2>Hello World</h2>");
-		sb.append("</body>");
-		sb.append("</html>");
-
-		return (new String(sb));
-	}
-
-	/**
-	 * Write sample HTML
-	 * 
-	 * @param resp
-	 */
-	protected void writeSampleHTML(HttpServletResponse resp) {
-		resp.setCharacterEncoding(StringPool.UTF8);
-		resp.setContentType(ContentTypes.TEXT_HTML_UTF8);
-		resp.setStatus(HttpServletResponse.SC_OK);
-
-		try {
-			ServletResponseUtil.write(resp, generateSampleHTML());
-		} catch (Exception e) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(e, e);
-			}
-
-			resp.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
-		}		
-	}
-	
 	@Override
 	public void init() throws ServletException {
 		if (_log.isInfoEnabled()) {
@@ -85,75 +62,148 @@ public class BladeHttpServlet extends HttpServlet {
 		if (_log.isInfoEnabled()) {
 			_log.info("init2");
 		}
+
 		super.init(config);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void service(ServletRequest request, ServletResponse response)
+		throws IOException, ServletException {
+
 		if (_log.isInfoEnabled()) {
-			_log.info("doPost");
+			_log.info("service2");
 		}
-		writeSampleHTML(response);
+
+		super.service(request, response);
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
 		if (_log.isInfoEnabled()) {
 			_log.info("doGet");
 		}
+
 		writeSampleHTML(response);
 	}
 
 	@Override
-	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doHead(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
 		if (_log.isInfoEnabled()) {
 			_log.info("doHead");
 		}
+
 		super.doHead(request, response);
 	}
 
 	@Override
-	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doOptions(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
 		if (_log.isInfoEnabled()) {
 			_log.info("doOptions");
 		}
+
 		super.doOptions(request, response);
 	}
 
 	@Override
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
+		if (_log.isInfoEnabled()) {
+			_log.info("doPost");
+		}
+
+		writeSampleHTML(response);
+	}
+
+	@Override
+	protected void doPut(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
 		if (_log.isInfoEnabled()) {
 			_log.info("doPut");
 		}
+
 		super.doPut(request, response);
 	}
 
 	@Override
-	protected void doTrace(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doTrace(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
 		if (_log.isInfoEnabled()) {
 			_log.info("doTrace");
 		}
+
 		super.doTrace(request, response);
 	}
 
+	/**
+	 * Dummy contents
+	 *
+	 * @return dummy contents string
+	 */
+	protected String generateSampleHTML() {
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("<html>");
+		sb.append("<head><title>Sample HTML</title></head>");
+		sb.append("<body>");
+		sb.append("<h2>Hello World</h2>");
+		sb.append("</body>");
+		sb.append("</html>");
+
+		return new String(sb);
+	}
+
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
 		if (_log.isInfoEnabled()) {
 			_log.info("service1");
 		}
+
 		super.service(request, response);
 	}
 
-	@Override
-	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-		if (_log.isInfoEnabled()) {
-			_log.info("service2");
+	/**
+	 * Write sample HTML
+	 *
+	 * @param resp
+	 */
+	protected void writeSampleHTML(HttpServletResponse resp) {
+		resp.setCharacterEncoding(StringPool.UTF8);
+		resp.setContentType(ContentTypes.TEXT_HTML_UTF8);
+		resp.setStatus(HttpServletResponse.SC_OK);
+
+		try {
+			ServletResponseUtil.write(resp, generateSampleHTML());
 		}
-		super.service(request, response);
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(e, e);
+			}
+
+			resp.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED);
+		}
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(BladeHttpServlet.class);
+	private static final Log _log = LogFactoryUtil.getLog(
+		BladeHttpServlet.class);
 
 	private static final long serialVersionUID = 1L;
+
 }
