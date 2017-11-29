65e73052287edd5ee0b4f63190581f7f387fbf8f
Lawrence Lee <lawrence.lee@liferay.com>
2017-11-29 10:25:51 -0600

    increase jvm for tomcat in CI

---
+++ liferay-workspace/configs/common/tomcat-8.0.32/bin/setenv.sh
@@ -0,0 +1,1 @@
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xms1024m -Xmx2048m"
