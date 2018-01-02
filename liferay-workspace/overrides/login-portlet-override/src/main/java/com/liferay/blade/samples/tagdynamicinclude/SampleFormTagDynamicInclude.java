deeb1d0c205f080667a5bc9f4e989e8fe7f1bb8e
Lawrence Lee <lawrence.lee@liferay.com>
2018-01-02 16:18:58 -0600

    auto SF

--- liferay-workspace/overrides/login-portlet-override/src/main/java/com/liferay/blade/samples/tagdynamicinclude/SampleFormTagDynamicInclude.java
+++ liferay-workspace/overrides/login-portlet-override/src/main/java/com/liferay/blade/samples/tagdynamicinclude/SampleFormTagDynamicInclude.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.samples.tagdynamicinclude;
 
 import com.liferay.portal.kernel.servlet.taglib.TagDynamicInclude;
@@ -15,7 +31,7 @@ import org.osgi.service.component.annotations.Component;
  * @author Carlos Sierra Andrés
  */
 @Component(immediate = true, service = TagDynamicInclude.class)
-public class SampleFormTagDynamicInclude implements TagDynamicInclude{
+public class SampleFormTagDynamicInclude implements TagDynamicInclude {
 
 	public void include(
 			HttpServletRequest request, HttpServletResponse response,
@@ -37,4 +53,5 @@ public class SampleFormTagDynamicInclude implements TagDynamicInclude{
 			"com.liferay.taglib.aui.FormTag",
 			PortletKeys.LOGIN + "-loginFormModal", "doStartTag#before");
 	}
+
 }
