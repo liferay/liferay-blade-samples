c0f53129158014b28587830347825d49ef107a7c
Lawrence Lee <lawrence.lee@liferay.com>
2018-05-18 21:11:36 -0500

    BLADE-242 format source

--- liferay-workspace/apps/service-builder/adq/adq-service/src/main/java/com/liferay/blade/samples/servicebuilder/adq/service/impl/BarLocalServiceImpl.java
+++ liferay-workspace/apps/service-builder/adq/adq-service/src/main/java/com/liferay/blade/samples/servicebuilder/adq/service/impl/BarLocalServiceImpl.java
@@ -1,15 +1,17 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * Copyright 2000-present Liferay, Inc.
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.liferay.blade.samples.servicebuilder.adq.service.impl;
@@ -57,27 +59,30 @@ public class BarLocalServiceImpl extends BarLocalServiceBaseImpl {
 		ActionableDynamicQuery adq = getActionableDynamicQuery();
 
 		adq.setAddCriteriaMethod(
-		new ActionableDynamicQuery.AddCriteriaMethod() {
+			new ActionableDynamicQuery.AddCriteriaMethod() {
 
-			@Override
-			public void addCriteria(DynamicQuery dynamicQuery) {
-				dynamicQuery.add(RestrictionsFactoryUtil.lt("field3", 100));
-			}
+				@Override
+				public void addCriteria(DynamicQuery dynamicQuery) {
+					dynamicQuery.add(RestrictionsFactoryUtil.lt("field3", 100));
+				}
 
-		});
+			});
 
 		adq.setPerformActionMethod(
-		new ActionableDynamicQuery.PerformActionMethod<Bar>() {
+			new ActionableDynamicQuery.PerformActionMethod<Bar>() {
 
-			@Override
-			public void performAction(Bar bar) {
-				int field3 = bar.getField3();
-				field3++;
-				bar.setField3(field3);
-				updateBar(bar);
-			}
+				@Override
+				public void performAction(Bar bar) {
+					int field3 = bar.getField3();
 
-		});
+					field3++;
+
+					bar.setField3(field3);
+
+					updateBar(bar);
+				}
+
+			});
 
 		try {
 			adq.performActions();
