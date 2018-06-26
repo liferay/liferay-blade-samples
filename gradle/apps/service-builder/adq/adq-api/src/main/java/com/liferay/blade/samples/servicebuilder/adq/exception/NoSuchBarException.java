42601bbc2281454dfc13e021a9254e0a0aaf7178
Lawrence Lee <lawrence.lee@liferay.com>
2018-06-26 12:33:36 -0500

    replicate sources

--- gradle/apps/service-builder/adq/adq-api/src/main/java/com/liferay/blade/samples/servicebuilder/adq/exception/NoSuchBarException.java
+++ gradle/apps/service-builder/adq/adq-api/src/main/java/com/liferay/blade/samples/servicebuilder/adq/exception/NoSuchBarException.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.liferay.blade.samples.servicebuilder.adq.exception;
 
 import aQute.bnd.annotation.ProviderType;
