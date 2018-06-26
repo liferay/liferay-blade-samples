1e052cf3ceac69ebcab8e037147b00e430cb0544
Lawrence Lee <lawrence.lee@liferay.com>
2018-06-26 12:39:55 -0500

    format source

--- liferay-workspace/apps/service-builder/adq/adq-api/src/main/java/com/liferay/blade/samples/servicebuilder/adq/exception/NoSuchBarException.java
+++ liferay-workspace/apps/service-builder/adq/adq-api/src/main/java/com/liferay/blade/samples/servicebuilder/adq/exception/NoSuchBarException.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.liferay.blade.samples.servicebuilder.adq.exception;
 
 import aQute.bnd.annotation.ProviderType;
