4d73cf0e44807d7dc4ef23db7ac04c192973f18a
Unknown <luanmaoski@gmail.com>
2018-07-17 16:20:51 -0300

    Fix sourceFormat

--- gradle/extensions/model-summary-contributor/src/main/java/com/liferay/blade/samples/modelsummarycontributor/ModelSummaryContributor.java
+++ gradle/extensions/model-summary-contributor/src/main/java/com/liferay/blade/samples/modelsummarycontributor/ModelSummaryContributor.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.samples.modelsummarycontributor;
 
 import com.liferay.petra.string.StringPool;
@@ -6,17 +22,21 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Summary;
+
+import java.util.Locale;
+
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 
-import java.util.Locale;
-
+/**
+ * @author Liferay
+ */
 @Component(
-		immediate = true,
-		property = "indexer.class.name=com.liferay.blogs.model.BlogsEntry",
-		service = com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor.class
+	immediate = true,
+	property = "indexer.class.name=com.liferay.blogs.model.BlogsEntry",
+	service = com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor.class
 )
 public class ModelSummaryContributor
 	implements com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor {
@@ -35,23 +55,25 @@ public class ModelSummaryContributor
 	public Summary getSummary(
 		Document document, Locale locale, String snippet) {
 
-		Summary summary = createSummary(document);
+		Summary summary = _createSummary(document);
 
 		summary.setMaxContentLength(128);
 
 		return summary;
 	}
 
-	private Summary createSummary(Document document) {
+	private Summary _createSummary(Document document) {
 		String prefix = Field.SNIPPET + StringPool.UNDERLINE;
 
 		String title = document.get(prefix + Field.TITLE, Field.TITLE);
 
-		String company = document.get(prefix + Field.COMPANY_ID, Field.COMPANY_ID);
+		String company = document.get(
+			prefix + Field.COMPANY_ID, Field.COMPANY_ID);
 
 		return new Summary(title, company);
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(
-			ModelSummaryContributor.class);
+		ModelSummaryContributor.class);
+
 }
--- gradle/extensions/model-summary-contributor/src/main/java/com/liferay/blade/samples/modelsummarycontributor/ModelSummayContributorRegistrar.java
+++ gradle/extensions/model-summary-contributor/src/main/java/com/liferay/blade/samples/modelsummarycontributor/ModelSummayContributorRegistrar.java
@@ -1,7 +1,25 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.samples.modelsummarycontributor;
 
+import com.liferay.blogs.kernel.model.BlogsEntry;
 import com.liferay.portal.search.spi.model.registrar.ModelSearchRegistrarHelper;
 import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
+
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
@@ -9,27 +27,35 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
-
+/**
+ * @author Liferay
+ */
 @Component(immediate = true)
 public class ModelSummayContributorRegistrar {
-    @Activate
-    protected void activate(BundleContext bundleContext) {
-        _serviceRegistration = modelSearchRegistrarHelper.register(com.liferay.blogs.model.BlogsEntry.class, bundleContext, modelSearchDefinition -> {
-            modelSearchDefinition.setModelSummaryContributor(
-                    modelSummaryContributor);
-        });
-    }
 
-    @Deactivate
-    protected void deactivate() {
-        _serviceRegistration.unregister();
-    }
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceRegistration = modelSearchRegistrarHelper.register(
+			BlogsEntry.class, bundleContext,
+			modelSearchDefinition -> {
+				modelSearchDefinition.setModelSummaryContributor(
+					modelSummaryContributor);
+			});
+	}
 
-    @Reference
-    protected ModelSearchRegistrarHelper modelSearchRegistrarHelper;
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
 
-    @Reference(target = "(indexer.class.name=com.liferay.blogs.model.BlogsEntry)")
-    protected ModelSummaryContributor modelSummaryContributor;
+	@Reference
+	protected ModelSearchRegistrarHelper modelSearchRegistrarHelper;
 
-    private ServiceRegistration<?> _serviceRegistration;
+	@Reference(
+		target = "(indexer.class.name=com.liferay.blogs.model.BlogsEntry)"
+	)
+	protected ModelSummaryContributor modelSummaryContributor;
+
+	private ServiceRegistration<?> _serviceRegistration;
+
 }
