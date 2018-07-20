8720f78f608696d652c08b4cc7ff541b3a53d8d8
Unknown <luanmaoski@gmail.com>
2018-07-20 14:13:00 -0300

    Add sample decription to README.markdown on project root

--- liferay-workspace/apps/service-builder/jndi/jndi-service/src/testIntegration/java/com/liferay/blade/samples/jndiservicebuilder/test/ConnectionTest.java
+++ liferay-workspace/apps/service-builder/jndi/jndi-service/src/testIntegration/java/com/liferay/blade/samples/jndiservicebuilder/test/ConnectionTest.java
@@ -1,7 +1,24 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.samples.jndiservicebuilder.test;
 
 import aQute.remote.util.JMXBundleDeployer;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.blade.samples.jndiservicebuilder.service.RegionLocalServiceUtil;
 
 import java.io.File;
@@ -14,7 +31,6 @@ import java.sql.Statement;
 import org.hsqldb.persist.HsqlProperties;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
@@ -58,7 +74,8 @@ public class ConnectionTest {
 
 	private static void _addData() throws Exception {
 		_executeNonQuery(
-			"create table region(id bigint not null primary key, name varchar(255));");
+			"create table region" +
+				"(id bigint not null primary key, name varchar(255));");
 		_executeNonQuery("insert into region(id, name) values(1, 'Tasmania');");
 	}
 
