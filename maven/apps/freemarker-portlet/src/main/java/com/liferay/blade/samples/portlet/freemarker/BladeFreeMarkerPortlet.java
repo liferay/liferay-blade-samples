356d41018cb9cc04644fb74897ae5baa1e0992aa
Lawrence Lee <lawrence.lee@liferay.com>
2019-01-02 16:11:59 -0600

    LPS-88036 Add portlet 3 configuration to portlet class

--- maven/apps/freemarker-portlet/src/main/java/com/liferay/blade/samples/portlet/freemarker/BladeFreeMarkerPortlet.java
+++ maven/apps/freemarker-portlet/src/main/java/com/liferay/blade/samples/portlet/freemarker/BladeFreeMarkerPortlet.java
@@ -36,7 +36,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/templates/view.ftl",
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
