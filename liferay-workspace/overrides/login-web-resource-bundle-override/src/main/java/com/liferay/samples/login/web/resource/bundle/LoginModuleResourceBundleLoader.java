b3d9f173b2021d212183d8fa35eddf87c2381016
Lawrence Lee <lawrence.lee@liferay.com>
2019-02-05 16:19:57 -0600

    Login Web Override Sample

---
+++ liferay-workspace/overrides/login-web-resource-bundle-override/src/main/java/com/liferay/samples/login/web/resource/bundle/LoginModuleResourceBundleLoader.java
@@ -0,0 +1,48 @@
+package com.liferay.samples.login.web.resource.bundle;
+
+import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
+import com.liferay.portal.kernel.util.CacheResourceBundleLoader;
+import com.liferay.portal.kernel.util.ClassResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author blah
+ */
+@Component(
+	immediate = true,
+	property = {
+		"bundle.symbolic.name=com.liferay.login.web",
+		"resource.bundle.base.name=content.Language",
+		"servlet.context.name=login-web"
+	}
+)
+public class LoginModuleResourceBundleLoader implements ResourceBundleLoader {
+
+	@Override
+	public ResourceBundle loadResourceBundle(Locale locale) {
+		return _resourceBundleLoader.loadResourceBundle(locale);
+	}
+
+	@Reference(
+		target = "(&(bundle.symbolic.name=com.liferay.login.web)(!(component.name=com.liferay.samples.login.web.resource.bundle.LoginModuleResourceBundleLoader)))"
+	)
+	public void setResourceBundleLoader(
+		ResourceBundleLoader resourceBundleLoader) {
+
+		_resourceBundleLoader = new AggregateResourceBundleLoader(
+			new CacheResourceBundleLoader(
+				new ClassResourceBundleLoader(
+					"content.Language",
+					LoginModuleResourceBundleLoader.class.getClassLoader())),
+			resourceBundleLoader);
+	}
+
+	private AggregateResourceBundleLoader _resourceBundleLoader;
+
+}
