a0fc572543516f756f4517ed630536539b749f48
Lawrence Lee <lawrence.lee@liferay.com>
2019-04-19 12:18:10 -0500

    BLADE-428 Add Hello Soy Portlet Sample

---
+++ liferay-workspace/apps/hello-soy-portlet/src/main/java/com/liferay/hello/soy/web/internal/portlet/HelloSoyPortletRegister.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.hello.soy.web.internal.portlet;
+
+import com.liferay.portal.portlet.bridge.soy.SoyPortletRegister;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Bruno Basto
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.application-type=full-page-application",
+		"com.liferay.portlet.application-type=widget",
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.scopeable=true",
+		"com.liferay.portlet.single-page-application=false",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Hello Soy Portlet",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=View",
+		"javax.portlet.name=hello_soy_portlet",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = SoyPortletRegister.class
+)
+public class HelloSoyPortletRegister implements SoyPortletRegister {
+}
