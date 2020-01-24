70290a2000fffaa5f28e18ceebf66c1240e2df33
Lawrence Lee <lawrence.lee@liferay.com>
2020-01-24 10:14:35 -0600

    Auto SF

--- liferay-workspace/extensions/servlet-filter/src/main/java/com/liferay/blade/samples/servletfilter/BladeServletFilter.java
+++ liferay-workspace/extensions/servlet-filter/src/main/java/com/liferay/blade/samples/servletfilter/BladeServletFilter.java
@@ -15,15 +15,16 @@ import org.osgi.service.component.annotations.Component;
  * Dynamically deployable ServletFilter sample.
  * Observe the comments all over this module for more information and
  * TODOs when you create your own filter from this template
- * 
+ *
  * @author Olaf Kock
  */
-
 @Component(
 		immediate = true,
 		property = {
+
 // To figure out valid values for before-filter, look up Liferay's
 // WEB-INF/liferay-web.xml
+
 				"before-filter=Auto Login Filter",
 				"dispatcher=REQUEST",
 //				"dispatcher=FORWARD",
@@ -31,34 +32,42 @@ import org.osgi.service.component.annotations.Component;
 //				"dispatcher=ERROR",
 //				"dispatcher=INCLUDE",
 				"servlet-context-name=",
+
 				// Pick your own unique filter name! Make sure to pick a
 				// different one for every filter you write - see LPS-107575
+
 				"servlet-filter-name=Blade Servlet Filter",
 				"url-pattern=/web/*",
 				"url-pattern=/change/me"
 		},
 		service=Filter.class
 )
-
 public class BladeServletFilter extends BaseFilter {
-	
-	@Override
-	protected void processFilter(HttpServletRequest httpServletRequest, 
-			HttpServletResponse httpServletResponse, FilterChain filterChain)
-					throws Exception {
-		// implement your own logic here
-		// determine if you shortcut, or continue in the filter chain
-		String requestURI = httpServletRequest.getRequestURI();
-		httpServletResponse.addHeader("X-Blade-Servlet-Filter", requestURI);
-		
-		processFilter(BladeServletFilter.class.getName(), httpServletRequest, 
-				httpServletResponse, filterChain);
-	}
 
 	@Override
 	protected Log getLog() {
 		return _log;
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(BladeServletFilter.class);
+	@Override
+	protected void processFilter(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, FilterChain filterChain)
+		throws Exception {
+
+		// implement your own logic here
+		// determine if you shortcut, or continue in the filter chain
+
+		String requestURI = httpServletRequest.getRequestURI();
+
+		httpServletResponse.addHeader("X-Blade-Servlet-Filter", requestURI);
+
+		processFilter(
+			BladeServletFilter.class.getName(), httpServletRequest,
+			httpServletResponse, filterChain);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BladeServletFilter.class);
+
 }
