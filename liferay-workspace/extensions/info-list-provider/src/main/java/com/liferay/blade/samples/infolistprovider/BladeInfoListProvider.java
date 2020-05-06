f7eabc67461a65e01ab2ccf0fea0f72171dc22e7
Lawrence Lee <lawrence.lee@liferay.com>
2020-05-06 14:01:22 -0500

    SF

--- liferay-workspace/extensions/info-list-provider/src/main/java/com/liferay/blade/samples/infolistprovider/BladeInfoListProvider.java
+++ liferay-workspace/extensions/info-list-provider/src/main/java/com/liferay/blade/samples/infolistprovider/BladeInfoListProvider.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liferay.blade.samples.infolistprovider;
 
 import com.liferay.asset.kernel.model.AssetEntry;
@@ -19,43 +35,54 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * Note: Prior to DXP 7.2 SP1, InfoListProvider was in a different package.
- * If you use that deprecated InfoListProvider class, your code won't work. 
- * 
+ * If you use that deprecated InfoListProvider class, your code won't work.
+ *
  * InfoListProviders appear on the UI as "Content Set Providers"
  *
  * @author Liferay Docs Team, Olaf Kock
- * @see https://portal.liferay.dev/docs/7-2/frameworks/-/knowledge_base/f/creating-an-information-list-provider
+ * @see <a href="https://portal.liferay.dev/docs/7-2/frameworks/-/knowledge_base/f/creating-an-information-list-provider">Creating an information list provider</a>
  */
-
 @Component(service = InfoListProvider.class)
-
 public class BladeInfoListProvider implements InfoListProvider<AssetEntry> {
+
 	@Override
-	public List<AssetEntry> getInfoList(InfoListProviderContext infoListProviderContext) {
-		return _assetEntryLocalService.getTopViewedEntries(new String[0], false, 0, 20);
+	public List<AssetEntry> getInfoList(
+		InfoListProviderContext infoListProviderContext) {
+
+		return _assetEntryLocalService.getTopViewedEntries(
+			new String[0], false, 0, 20);
 	}
 
 	@Override
-	public List<AssetEntry> getInfoList(InfoListProviderContext infoListProviderContext, Pagination pagination,
-			Sort sort) {
-		return _assetEntryLocalService.getTopViewedEntries(new String[0], !sort.isReverse(), pagination.getStart(),
-				pagination.getEnd());
+	public List<AssetEntry> getInfoList(
+		InfoListProviderContext infoListProviderContext, Pagination pagination,
+		Sort sort) {
+
+		return _assetEntryLocalService.getTopViewedEntries(
+			new String[0], !sort.isReverse(), pagination.getStart(),
+			pagination.getEnd());
 	}
 
 	@Override
-	public int getInfoListCount(InfoListProviderContext infoListProviderContext) {
+	public int getInfoListCount(
+		InfoListProviderContext infoListProviderContext) {
 
 		Company company = infoListProviderContext.getCompany();
 
-		return _assetEntryLocalService.getCompanyEntriesCount(company.getCompanyId());
+		return _assetEntryLocalService.getCompanyEntriesCount(
+			company.getCompanyId());
 	}
 
 	@Override
 	public String getLabel(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle("content.Language", locale, getClass());
-		return LanguageUtil.get(resourceBundle, "blade-info-list-provider-label");
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(
+			resourceBundle, "blade-info-list-provider-label");
 	}
 
 	@Reference
-	AssetEntryLocalService _assetEntryLocalService;
+	private AssetEntryLocalService _assetEntryLocalService;
+
 }
