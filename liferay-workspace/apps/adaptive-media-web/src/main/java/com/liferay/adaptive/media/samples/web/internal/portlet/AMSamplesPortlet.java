7132d19490f9dea854ebb1872e0d1b3cadfe77b7
Lawrence Lee <lawrence.lee@liferay.com>
2020-07-20 14:45:17 -0500

    Merge pr-114

---
+++ liferay-workspace/apps/adaptive-media-web/src/main/java/com/liferay/adaptive/media/samples/web/internal/portlet/AMSamplesPortlet.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.adaptive.media.samples.web.internal.portlet;
+
+import com.liferay.adaptive.media.samples.web.internal.constants.AMSamplesPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-adaptive-media-samples",
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.layout-cacheable=true",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.remoteable=true",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Adaptive Media Samples",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.always-display-default-configuration-icons=true",
+		"javax.portlet.name=" + AMSamplesPortletKeys.ADAPTIVE_MEDIA_SAMPLES,
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class AMSamplesPortlet extends MVCPortlet {
+}
---
+++ maven/apps/adaptive-media-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,50 @@
+<%--
+/**
+ * Copyright 2000-present Liferay, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<div class="container">
+
+<%
+String[] mimeTypes = {"image/bmp", "image/gif", "image/jpeg", "image/pjpeg", "image/png", "image/tiff", "image/x-citrix-jpeg", "image/x-citrix-png", "image/x-ms-bmp", "image/x-png", "image/x-tiff"};
+
+List<FileEntry> fileEntries = DLAppServiceUtil.getFileEntries(scopeGroupId, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, mimeTypes);
+
+int columns = 0;
+
+for (FileEntry fileEntry : fileEntries) {
+%>
+
+	<c:if test="<%= (columns % 2) == 0 %>">
+		<c:if test="<%= columns != 0 %>">
+			</div>
+		</c:if>
+
+		<div class="row">
+	</c:if>
+
+	<div class="col-md-6">
+		<liferay-adaptive-media:img class="img-fluid" fileVersion="<%= fileEntry.getFileVersion() %>" />
+	</div>
+
+	<%
+	columns++;
+}
+%>
+
+</div>
