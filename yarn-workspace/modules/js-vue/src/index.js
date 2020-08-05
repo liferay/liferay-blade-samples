5866d73b4703979aa496662e53827c402d0eed9b
Lawrence Lee <lawrence.lee@liferay.com>
2020-08-05 16:50:00 -0500

    BLADE-590 Import class from other project

--- yarn-workspace/modules/js-vue/src/index.js
+++ yarn-workspace/modules/js-vue/src/index.js
@@ -1,3 +1,4 @@
+import AppComponent from 'js-react';
 
 import Vue from 'vue/dist/vue.common';
 
@@ -5,21 +6,21 @@ import Vue from 'vue/dist/vue.common';
 /**
  * This is the main entry point of the portlet.
  *
- * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent 
+ * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent
  * information on the signature of this function.
  *
  * @param  {Object} params a hash with values of interest to the portlet
  * @return {void}
  */
 export default function main({portletNamespace, contextPath, portletElementId, configuration}) {
-    
+
     const node = document.getElementById(portletElementId);
-    
+
     // Dynamically write markup to portlet's node
     node.innerHTML = `
         <div>
             <div>
-                <span class="tag">Portlet Namespace:</span> 
+                <span class="tag">Portlet Namespace:</span>
                 <span class="value">{{portletNamespace}}</span>
             </div>
             <div>
@@ -30,17 +31,17 @@ export default function main({portletNamespace, contextPath, portletElementId, c
                 <span class="tag">Portlet Element Id:</span>
                 <span class="value">{{portletElementId}}</span>
             </div>
-            
+
             <div>
                 <span class="tag">Configuration:</span>
                 <span class="value pre">{{JSON.stringify(configuration, null, 2)}}</span>
             </div>
-            
+
         </div>
     `;
-    
+
     //
-    // Use runtime + compiler module in this case so that we don't need to 
+    // Use runtime + compiler module in this case so that we don't need to
     // process templates during build time.
     //
     // See https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
@@ -52,5 +53,5 @@ export default function main({portletNamespace, contextPath, portletElementId, c
 			portletNamespace, contextPath, portletElementId, configuration
 		}
 	});
-    
+
 }
